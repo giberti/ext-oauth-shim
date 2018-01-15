532cce87e6c290ff530e463cf1d9693a6c0084d7
Erik Giberti <erik@af-design.com>
2018-01-15 14:19:39 -0600

    Reordering methods and applying PSR-2

--- tests/functionsTest.php
+++ tests/functionsTest.php
@@ -2,59 +2,61 @@
 
 use PHPUnit\Framework\TestCase;
 
-class FunctionsTest extends TestCase {
+class FunctionsTest extends TestCase
+{
 
-    public function provide_oauth_get_sbs() {
+    public function provide_oauth_get_sbs()
+    {
         return [
-            'basic string generation' => [
+            'basic string generation'                  => [
                 'method',
                 'http://example.com/',
                 [], // params
-                'method&http%3A%2F%2Fexample.com%2F&'
+                'method&http%3A%2F%2Fexample.com%2F&',
             ],
-            'method case preserved' => [
+            'method case preserved'                    => [
                 'MeThOd',
                 'http://example.com/',
                 [], // params
-                'MeThOd&http%3A%2F%2Fexample.com%2F&'
+                'MeThOd&http%3A%2F%2Fexample.com%2F&',
             ],
             'scheme and host lower case, but not path' => [
                 'method',
                 'HTTP://EXAMPLE.COM/FOO.PHP',
                 [],
-                'method&http%3A%2F%2Fexample.com%2FFOO.PHP&'
+                'method&http%3A%2F%2Fexample.com%2FFOO.PHP&',
             ],
-            'specified port numbers work' => [
+            'specified port numbers work'              => [
                 'method',
                 'http://example.com:8888/',
                 [],
-                'method&http%3A%2F%2Fexample.com%3A8888%2F&'
+                'method&http%3A%2F%2Fexample.com%3A8888%2F&',
             ],
-            'ipv4 host names work' => [
+            'ipv4 host names work'                     => [
                 'method',
                 'http://127.0.0.1/',
                 [],
-                'method&http%3A%2F%2F127.0.0.1%2F&'
+                'method&http%3A%2F%2F127.0.0.1%2F&',
             ],
-            'ipv4 host names with a port work' => [
+            'ipv4 host names with a port work'         => [
                 'method',
                 'http://127.0.0.1:8888/',
                 [],
-                'method&http%3A%2F%2F127.0.0.1%3A8888%2F&'
+                'method&http%3A%2F%2F127.0.0.1%3A8888%2F&',
             ],
-            'ipv6 host names work' => [
+            'ipv6 host names work'                     => [
                 'method',
                 'http://::1/',
                 [],
-                'method&http%3A%2F%2F%3A%3A1%2F&'
+                'method&http%3A%2F%2F%3A%3A1%2F&',
             ],
-            'ipv6 host names with a port work' => [
+            'ipv6 host names with a port work'         => [
                 'method',
                 'http://::1:8888/',
                 [],
-                'method&http%3A%2F%2F%3A%3A1%3A8888%2F&'
+                'method&http%3A%2F%2F%3A%3A1%3A8888%2F&',
             ],
-            'params encode properly' => [
+            'params encode properly'                   => [
                 'method',
                 'http://example.com/',
                 [
@@ -63,9 +65,9 @@ class FunctionsTest extends TestCase {
                     'k3' => '{}',
                     'k4' => '%',
                 ],
-                'method&http%3A%2F%2Fexample.com%2F&k1%3Dv1%26k2%3D%25241%26k3%3D%257B%257D%26k4%3D%2525'
+                'method&http%3A%2F%2Fexample.com%2F&k1%3Dv1%26k2%3D%25241%26k3%3D%257B%257D%26k4%3D%2525',
             ],
-            'params sort correctly' => [
+            'params sort correctly'                    => [
                 'method',
                 'http://example.com/',
                 [
@@ -74,16 +76,16 @@ class FunctionsTest extends TestCase {
                     'k3' => '{}',
                     'k1' => 'v1',
                 ],
-                'method&http%3A%2F%2Fexample.com%2F&k1%3Dv1%26k2%3D%25241%26k3%3D%257B%257D%26k4%3D%2525'
+                'method&http%3A%2F%2Fexample.com%2F&k1%3Dv1%26k2%3D%25241%26k3%3D%257B%257D%26k4%3D%2525',
             ],
-            'mixed parameters and query string' => [
+            'mixed parameters and query string'        => [
                 'method',
                 'http://example.com/?k1=v1',
                 [
                     'k2' => 'k2',
                 ],
-                'method&http%3A%2F%2Fexample.com%2F&k1%3Dv1%26k2%3Dk2'
-            ]
+                'method&http%3A%2F%2Fexample.com%2F&k1%3Dv1%26k2%3Dk2',
+            ],
         ];
     }
 
@@ -92,37 +94,41 @@ class FunctionsTest extends TestCase {
      *
      * @dataProvider provide_oauth_get_sbs
      */
-    public function test_oauth_get_sbs($method, $uri, $params, $expected) {
+    public function test_oauth_get_sbs($method, $uri, $params, $expected)
+    {
         $actual = oauth_get_sbs($method, $uri, $params);
         $this->assertEquals($expected, $actual, 'Incorrect signature base string');
+    }
+
+    public function provide_invalid_urls()
+    {
+        return [
+            'Missing path'        => ['http://example.com'],
+            'Relative path'       => ['/api/example'],
+            'Schema-less url'     => ['//example.com/'],
+            'Schema-less url two' => ['://example.com/'],
+        ];
     }
 
     /**
      * @dataProvider provide_invalid_urls
      */
-    public function test_oauth_get_sbs_fails_invalid_urls($url) {
+    public function test_oauth_get_sbs_fails_invalid_urls($url)
+    {
         $this->expectException(OAuthException::class);
         oauth_get_sbs('method', $url, []);
     }
 
-    public function provide_invalid_urls() {
+    public function provide_oauth_urlencode()
+    {
         return [
-            'Missing path' => ['http://example.com'],
-            'Relative path' => ['/api/example'],
-            'Schema-less url' => ['//example.com/'],
-            'Schema-less url two' => ['://example.com/'],
-        ];
-    }
-
-    public function provide_oauth_urlencode() {
-        return [
-            'properly encode scheme://host:port' => [
+            'properly encode scheme://host:port'                 => [
                 'scheme://example.com:8088/',
-                'scheme%3A%2F%2Fexample.com%3A8088%2F'
+                'scheme%3A%2F%2Fexample.com%3A8088%2F',
             ],
             'properly handle tilde, bang, equals, and ampersand' => [
                 'http://foo.bar/~baz!baz?foo=bar',
-                'http%3A%2F%2Ffoo.bar%2F~baz%21baz%3Ffoo%3Dbar'
+                'http%3A%2F%2Ffoo.bar%2F~baz%21baz%3Ffoo%3Dbar',
             ],
         ];
     }
@@ -132,7 +138,8 @@ class FunctionsTest extends TestCase {
      *
      * @dataProvider provide_oauth_urlencode
      */
-    public function test_oauth_urlencode($uri, $expected) {
+    public function test_oauth_urlencode($uri, $expected)
+    {
         $this->assertEquals($expected, oauth_urlencode($uri), 'Incorrect encoding');
     }
 }
